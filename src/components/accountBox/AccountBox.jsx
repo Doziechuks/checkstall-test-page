ee0bb5ed0d654ea6f284263cee4eea27def911e4
doziechuks <doziechuks1010@gmail.com>
2023-05-04 10:30:12 +0100

    restructured the mininavbar component

--- src/components/accountBox/AccountBox.jsx
+++ src/components/accountBox/AccountBox.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import classes from './AccountBox.module.css';
+import React from "react";
+import classes from "./AccountBox.module.css";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FcManager } from "react-icons/fc";
 import { FaAngleDown } from "react-icons/fa";
 
 const AccountBox = ({ isMobile }) => {
   return (
-    <div className={`${classes.accountBox} ${isMobile && classes.mobileAccountBox}`}>
-      <div className={classes.account}>
-        <FcManager className={classes.accountIcon} />
-        <span>Account</span>
-        <FaAngleDown size={20} />
-      </div>
-      <div className={classes.cart}>
-        <AiOutlineShoppingCart  className={classes.cartIcon} />
-      </div>
+    <div
+      className={`${classes.accountBox} ${
+        isMobile && classes.mobileAccountBox
+      }`}
+    >
+      <FcManager className={classes.accountIcon} />
+      <span>account</span>
+      <FaAngleDown size={20} />
     </div>
   );
-}
+};
 
-export default AccountBox
+export default AccountBox;
--- src/components/miniNavbar/MiniNavbar.jsx
+++ src/components/miniNavbar/MiniNavbar.jsx
@@ -1,20 +1,55 @@
 import React from 'react';
 import classes from './MiniNavbar.module.css';
 import { IoMdCall } from "react-icons/io";
-import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineMail, AiOutlineShoppingCart } from "react-icons/ai";
+import AccountBox from '../accountBox/AccountBox';
 
 const MiniNavbar = () => {
   return (
     <nav className={classes.wrapper}>
       <main className={classes.container}>
-        <div className={classes.info}>
-          <IoMdCall />
-          <p>+234 980 939 3031</p>
+        <div className={classes.accountBox}>
+          <AccountBox />
         </div>
-        <span>|</span>
-        <div className={classes.info}>
-          <AiOutlineMail />
-          <p>support@checkstall.com</p>
+        <div className={classes.navInfo}>
+          <div className={classes.option}>
+            home
+            <span className={`${classes.underline} ${classes.active}`} />
+          </div>
+          <div className={classes.option}>
+            about us
+            <span className={classes.underline} />
+          </div>
+          <div className={classes.option}>
+            sell on checkstall
+            <span className={classes.underline} />
+          </div>
+          <div className={classes.option}>
+            buy from China
+            <span className={classes.underline} />
+          </div>
+          <div className={classes.option}>
+            help center
+            <span className={classes.underline} />
+          </div>
+        </div>
+        <div className={classes.cartBox}>
+          <div className={classes.iconBox}>
+            <AiOutlineShoppingCart className={classes.cartIcon} />
+            <span className={classes.circle}>0</span>
+          </div>
+          <span className={classes.shop}>shopping cart</span>
+        </div>
+        <div className={classes.contactBox}>
+          <div className={classes.info}>
+            <IoMdCall />
+            <p>+234 980 939 3031</p>
+          </div>
+          <span>|</span>
+          <div className={classes.info}>
+            <AiOutlineMail />
+            <p>support@checkstall.com</p>
+          </div>
         </div>
       </main>
     </nav>
